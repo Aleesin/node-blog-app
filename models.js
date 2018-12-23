973a80d23e42789351c83b608060ba0f48797a06
Sarah Burgess <sarahburgess287@gmail.com>
2018-12-23 11:34:14 -0800

    Updating the blogpost.Schema.methods in the models.js

--- models.js
+++ models.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 
 // Schema to represent Data models
 
+// Schema for blogposts
 const blogpostSchema = mongoose.Schema({
     title: { type: String, required: true },
     content: { type: String, required: true},
@@ -12,5 +13,25 @@ const blogpostSchema = mongoose.Schema({
         lastName: String, required: true,
     }
 
-})
+});
 
+// Virtuals: create author name string out of 2 firstName and lastName keys
+
+// instance methods: serialize to create blogpost object to return
+blogpostSchema.methods.serialize = function() {
+    return {
+        id: this.id,
+        title: this.title,
+        firstName: this.firstName,
+        lastName: this.lastName,
+        author: this.authorString,
+
+    }
+}
+
+// all instance methods and virtual properties on schema
+// must be defined before make the call to `.model`
+
+const Blogpost = mongoose.model("Blogpost", blogpostSchema);
+
+module.exports = { Blogpost };
