232ae75559e88dfb810284d5b64cd6e0bc783bee
Sarah Burgess <sarahburgess287@gmail.com>
2018-12-23 10:56:07 -0800

    Updating blogpostSchema

--- models.js
+++ models.js
@@ -2,5 +2,15 @@
 
 const mongoose = require("mongoose");
 
-// Schema to represent Blog posts
+// Schema to represent Data models
+
+const blogpostSchema = mongoose.Schema({
+    title: { type: String, required: true },
+    content: { type: String, required: true},
+    author: {
+        firstName: String, required: true,
+        lastName: String, required: true,
+    }
+
+})
 
