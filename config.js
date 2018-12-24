bb612ce167699c92782cf5b24522d0e5d53bff09
Sarah Burgess <sarahburgess287@gmail.com>
2018-12-23 19:23:54 -0800

    Added comment

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 "use strict";
-
+// make sure to use the name of the database from in the url
 exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/blogPostDb";
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || "mongodb://localhost/blogPostDb";
 exports.PORT = process.env.PORT || 8080;
