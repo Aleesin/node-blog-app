c4b85bbf8d1310cb34228d8b0b621e65fdd9a2db
Sarah Burgess <sarahburgess287@gmail.com>
2018-12-24 16:22:33 -0800

    edited linting

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 "use strict";
 // make sure to use the name of the database from in the url, does test need to point to different db?
+
 exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/blogPostDb";
+
 //exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || "mongodb://localhost/blogPostDbtest";
 exports.PORT = process.env.PORT || 8080;
